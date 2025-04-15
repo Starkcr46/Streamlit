f1282a19ada76fbbf948e85af7cf0cfd392cbc4b
Starkcr46 <starkcr39@gmail.com>
2025-04-15 07:22:17 -0500

    working database connection/query

--- database/main.py
+++ database/main.py
@@ -17,19 +17,25 @@ supabase = init_connection()
 # Query the db
 
 
-@st.cache_data(ttl=600)  # cache clears after 10 minutes
-def run_query():
+@st.cache_resource
+def run_query(table_name):
     # Return all data
-    return supabase.table('car_parts_monthly_sales').select("*").execute()
+    return supabase.table(table_name).select("*").execute()
 
     # Filter data
     # return supabase.table('car_parts_monthly_sales').select("*").eq("parts_id", 2674).execute()
 
-
-
+def get_row_count(table_name):
+    response = supabase.table(table_name).select("*", count="exact").execute()
+    return len(response.data) if response.data else 0
 
 st.title("Query a database")
-rows = run_query()
+
+table_name = "car_parts_monthly_sales"
+row_count = get_row_count(table_name)
+st.write(f"The table '{table_name}' has {row_count} rows.")
+
+rows = run_query(table_name)
 
 # Store in dataframe
 df = pd.json_normalize(rows.data)
